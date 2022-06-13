d430689fe2ad7f925577e030eb324ce1d75ca642
nycthuner <105286745+nycthuner@users.noreply.github.com>
2022-06-12 23:35:19 -0300

    rpg2.0

--- rpg.py
+++ rpg.py
@@ -1,83 +1,209 @@
 from random import randint as dano
-monstros = 20
+import os
 while True:
     personagem = input("Escolha um personagem: Mago ou Guerreiro: ")
     if not "Mago" in personagem and not "Guerreiro" in personagem:
         continue
     break
-
-mago = int(dano(7,15))
-guerreiro = int(dano(5,10))
+ap=dano(7,15)
+ad=dano(5,10)
+vida=15
+monstros = 20
+"""while personagem == "Mago" or personagem == :  #or continuar == "continuar"""
 if personagem == "Mago":
-    personagem = mago
-    while personagem > 0 and monstros > 0:
-        acao = input("Escolha sua acao: \n Atacar \n Defender \n Curar \n Descansar: ")
-        if not "Atacar" in acao or not "Defender" in acao or not "Curar" in acao or not "Descansar" in acao:
-            atacarmago = dano(0,8)
-            ataquemonstro = dano(0,10)
-            curar = dano(0,5)
-            descansar = dano(1,5)
-            if acao == "Atacar":
-                personagem -= 2
-                personagem -= ataquemonstro
-                monstros -= atacarmago
-                print("Sua vida: " ,personagem)
-                print("Dano do ataque foi: " ,atacarmago)
-                print("A vida do monstro: " ,monstros)
-                print("Ataque do monstro foi: " ,ataquemonstro)
-            elif acao == "Defender":
-                personagem -= 1
-                print("Sua vida: " ,personagem)
-                print("A vida do monstro: " ,monstros)
-            elif acao == "Curar":
-                personagem +=curar
-                personagem -=ataquemonstro
-                print("Sua vida: " ,personagem)
-                print("A vida do monstro: " ,monstros)
-                print("Ataque do monstro foi: " ,ataquemonstro)
-            elif acao == "Descansar":
-                personagem +=descansar
-                personagem -= ataquemonstro
-                print("A sua vida: " ,personagem)
-                print("A vida do monstro: " ,monstros)
-                print("Ataque do monstro foi: " ,ataquemonstro)
-            else:
-                print("Por favor digitar certo")
+        while vida >= 0 and monstros >= 0:
+            os.system("clear")
+            acao = input("Escolha uma acao: \n 1-Atacar \n 2-Defender \n 3-curar \n 4-descansar: ")
+            if not "1" in acao or not "2" in acao or not "3" in acao or not "4" in acao:
+                damage = dano(0,8)
+                monstrosdamage= dano(0,10)
+                curar = dano(0,5)
+                defesa = dano(0,7)
+                descanse = dano(0,5)
+                acaomonstro = dano(1,2)
+                if acao == "1":
+                    if ap>damage and acaomonstro==1:
+                        ap-=2
+                        vida-=monstrosdamage
+                        monstros-=damage
+                        print("Voce esta com" ,ap ,"ap")
+                        print("Seu ataque foi: " ,damage)
+                        print("O ataque do monstro: " ,monstrosdamage)
+                        print("Sua vida esta em: " ,vida)
+                        print("A vida do monstro esta em: " ,monstros)
+                    elif ap>damage and acaomonstro==2:
+                        ap-=2
+                        if defesa>damage:
+                            print("Voce esta com" ,ap ,"ap")
+                            print("Seu ataque foi defendido.")
+                            print("Sua vida: " ,vida)
+                            print("A vida do monstro: " ,monstros)
+                        else:
+                            monstros-=damage
+                            monstros+=defesa
+                            print("Voce esta com" ,ap ,"ap")
+                            print("Seu foi ataque meramente defendido")
+                            print("Dano no monstro: " ,damage)
+                            print("A defesa do montro: " ,defesa)
+                            print("Sua vida: " ,vida)
+                            print("A vida do monstro: " ,monstros)
+                    elif damage>ap and acaomonstro==2:
+                            print("Voce nao tem ap suficiente, descanse")
+                            print("Sorte sua o monstro escolheu defender")
+                            print("Sua vida: " ,vida)
+                            print("A vida do monstro: " ,monstros)
+                    else:
+                        vida-=monstrosdamage
+                        print("Voce nao tem ap suficiente, descanse")
+                        print("Sofreu dano do monstro: " ,monstrosdamage)
+                        print("Sua vida: " ,vida)
+                        print("A vida do monstro: " ,monstros)
+                elif acao =="2":
+                    if acaomonstro ==1:
+                        if monstrosdamage>defesa:
+                            vida-=monstrosdamage
+                            vida+=defesa
+                            print("Sua defesa nao foi suficiente")
+                            print("Dano do monstro: " ,monstrosdamage)
+                            print("Sua defesa: " ,defesa)
+                            print("Sua vida: " ,vida)
+                            print("A vida do monstro: " ,monstros)
+                        else:
+                            print("Sua defesa fez efeito")
+                            print("Sua vida: " ,vida)
+                            print("A vida do monstro: " ,monstros)
+                    else:
+                        print("Ambos defederam")
+                        print("Sua vida:" ,vida)
+                        print("A vida do monstro: " ,monstros)
+                elif acao =="3":
+                    if acaomonstro==1:
+                        vida-=monstrosdamage
+                        vida+=curar
+                        print("O dano do monstro: " ,monstrosdamage)
+                        print("Sua vida: " ,vida)
+                        print("A vida do montro:" ,monstros)
+                    else:
+                        vida+=curar
+                        print("Nao ocorreu nada")
+                        print("Sua vida: " ,vida)
+                        print("A vida do montro: " ,monstros)
+                elif acao =="4":
+                    if acaomonstro==1:
+                        ap+=descanse
+                        vida -= monstrosdamage
+                        print("Voce recuperou um pouco de ap")
+                        print("voce tem " ,ap ,"de ap")
+                        print("O dano do montro foi de: " ,monstrosdamage)
+                        print("Sua vida: " ,vida)
+                        print("A vida do montro: " ,monstros)
+                    else:
+                        ap+=descanse
+                        print("Voce recuperou um pouco de ap")
+                        print("Voce tem " ,ap ,"de ap")
+                        print("O monstro escolheu se defender")
+                        print("Sua vida: " ,vida)
+                        print("A vida do monstro:" ,monstros)
+                else:
+                    print("Escreva o numero da acao corretamente.")
 else:
-    personagem = guerreiro
-    while personagem > 0 and monstros > 0:
-        acao = input("Escolha sua acao: \n Atacar \n Defender \n Curar \n Descansar: ")
-        if not "Atacar" in acao or not "Defender" in acao or not "Curar" in acao or not "Descansar" in acao:
-            atacarwarrior = dano(0,8)
-            ataquemonstro = dano(0,8)
-            descansar = dano(1,5)
-            if acao == "Atacar":
-                personagem -= 2
-                personagem -= ataquemonstro
-                monstros -= atacarwarrior
-                print("Sua vida: " ,personagem)
-                print("Dano do ataque: " ,atacarwarrior)
-                print("A vida do monstro: " ,monstros)
-                print("O dano do monstro foi: ")
-            elif acao == "Defender":
-                personagem -= 1
-                print("Sua vida: " ,personagem)
-                print("A vida do monstro: " ,monstros)
-            elif acao == "Curar":
-                personagem +=curar
-                personagem -= ataquemonstro
-                print("Sua vida: " ,personagem)
-                print("A vida do monstro: " ,monstros)
-                print("O dano do monstro foi: " ,ataquemonstro)
-            elif acao == "Descansar":
-                personagem +=descansar
-                personagem -= ataquemonstro
-                print("A sua vida: " ,personagem)
-                print("A vida do monstro: " ,monstros)
-                print("O ataque do monstro foi: " ,ataquemonstro)
-            else:
-                print("Por favor digitar certo")
-            
+        while vida >= 0 and monstros >= 0:
+            os.system("clear")
+            acao = input("Escolha uma acao: \n 1-Atacar \n 2-Defender \n 3-curar \n 4-descansar: ")
+            if not "1" in acao or not "2" in acao or not "3" in acao or not "4" in acao:
+                damage = dano(0,8)
+                monstrosdamage= dano(0,10)
+                curar = dano(0,5)
+                defesa = dano(0,7)
+                descanse = dano(0,5)
+                acaomonstro = dano(1,2)
+
+                if acao == "1":
+                    if ap>damage and acaomonstro==1:
+                        ap-=2
+                        vida-=monstrosdamage
+                        monstros-=damage
+                        print("Voce esta com" ,ap ,"ap")
+                        print("Seu ataque foi: " ,damage)
+                        print("O ataque do monstro: " ,monstrosdamage)
+                        print("Sua vida esta em: " ,vida)
+                        print("A vida do monstro esta em: " ,monstros)
+                    elif ap>damage and acaomonstro==2:
+                        ap-=2
+                        if defesa>damage:
+                            print("Voce esta com" ,ap ,"ap")
+                            print("Seu ataque foi defendido.")
+                            print("Sua vida: " ,vida)
+                            print("A vida do monstro: " ,monstros)
+                        else:
+                            monstros-=damage
+                            monstros+=defesa
+                            print("Voce esta com" ,ap ,"ap")
+                            print("Seu foi ataque meramente defendido")
+                            print("Dano no monstro: " ,damage)
+                            print("A defesa do montro: " ,defesa)
+                            print("Sua vida: " ,vida)
+                            print("A vida do monstro: " ,monstros)
+                    elif damage>ap and acaomonstro==2:
+                            print("Voce nao tem ap suficiente, descanse")
+                            print("Sorte sua o monstro escolheu defender")
+                            print("Sua vida: " ,vida)
+                            print("A vida do monstro: " ,monstros)
+                    else:
+                        vida-=monstrosdamage
+                        print("Voce nao tem ap suficiente, descanse")
+                        print("Sofreu dano do monstro: " ,monstrosdamage)
+                        print("Sua vida: " ,vida)
+                        print("A vida do monstro: " ,monstros)
+                elif acao =="2":
+                    if acaomonstro ==1:
+                        if monstrosdamage>defesa:
+                            vida-=monstrosdamage
+                            vida+=defesa
+                            print("Sua defesa nao foi suficiente")
+                            print("Dano do monstro: " ,monstrosdamage)
+                            print("Sua defesa: " ,defesa)
+                            print("Sua vida: " ,vida)
+                            print("A vida do monstro: " ,monstros)
+                        else:
+                            print("Sua defesa fez efeito")
+                            print("Sua vida: " ,vida)
+                            print("A vida do monstro: " ,monstros)
+                    else:
+                        print("Ambos defederam")
+                        print("Sua vida:" ,vida)
+                        print("A vida do monstro: " ,monstros)
+                elif acao =="3":
+                    if acaomonstro==1:
+                        vida-=monstrosdamage
+                        vida+=curar
+                        print("O dano do monstro: " ,monstrosdamage)
+                        print("Sua vida: " ,vida)
+                        print("A vida do montro:" ,monstros)
+                    else:
+                        vida+=curar
+                        print("Nao ocorreu nada")
+                        print("Sua vida: " ,vida)
+                        print("A vida do montro: " ,monstros)
+                elif acao =="4":
+                    if acaomonstro==1:
+                        ap+=descanse
+                        vida -= monstrosdamage
+                        print("Voce recuperou um pouco de ap")
+                        print("voce tem " ,ap ,"de ap")
+                        print("O dano do montro foi de: " ,monstrosdamage)
+                        print("Sua vida: " ,vida)
+                        print("A vida do montro: " ,monstros)
+                    else:
+                        ap+=descanse
+                        print("Voce recuperou um pouco de ap")
+                        print("Voce tem " ,ap ,"de ap")
+                        print("O monstro escolheu se defender")
+                        print("Sua vida: " ,vida)
+                        print("A vida do monstro:" ,monstros)
+                else:
+                    print("Digite o numero da acao corretamente.")
+os.system("clear")
 while True:
     continuar=input("Deseja continuar ou sair:")
     if not "continuar" in  continuar and not "sair" in continuar:
